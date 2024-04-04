463e565dff629ea9b6132e0c4a75298aa445159b
jsalazar8 <jsalazar8@hawk.iit.edu>
2024-04-03 22:29:48 -0500

    toggles the visibility of filter options

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-function toggleDropdown(id){}
+function toggleDropdown(id){
+    var dropdown = document.getElementById(id);
+    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+      dropdown.style.display = 'block';
+    } else {
+      dropdown.style.display = 'none';
+    }
+}
