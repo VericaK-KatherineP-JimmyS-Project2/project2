4982b7d627ccde5e8aa0d051eb97b6f56d6cc6c2
Kat Prush <kprush@hawk.iit.edu>
2024-04-25 23:00:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ function toggleDropdown(id){
       dropdown.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var modal = document.getElementById('signup-modal'); // Get the modal
+
+  // Attach a single event listener to the document body
+  document.body.addEventListener('click', function(event) {
+      // Check if the clicked element is the "Create a New Account" button
+      if (event.target.id === 'create-account-btn') {
+          modal.style.display = 'block'; // Open the modal
+      }
+
+      // Check if the clicked element is the close button in the modal
+      if (event.target.classList.contains('close')) {
+          modal.style.display = 'none'; // Close the modal
+      }
+
+      // Check if the clicked element is the modal itself (outside the modal content)
+      if (event.target === modal) {
+          modal.style.display = 'none'; // Close the modal
+      }
+  });
+
+
+});
